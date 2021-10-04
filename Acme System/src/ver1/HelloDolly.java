6abc7ef2aef17a8dd07cae02e635691f78a61d4a
Randall Pace <rbpace@valdosta.edu>
2021-10-04 16:43:09 -0400

    more changes to dolly

--- Acme System/src/ver1/HelloDolly.java
+++ Acme System/src/ver1/HelloDolly.java
@@ -2,7 +2,9 @@ package ver1;
 
 public class HelloDolly {
 	public static void main(String[]args) {
-		System.out.println("Hello Doly");
+		System.out.println("Hello Doly from Eclipse");
+		System.out.println("Hello Doly from Eclipse Gin");
+
 	}
 	
 }
